2a6ad788e26050cdd1b034e305c31b00ed97805a
KeKs0r <marc.hoeffl@laserfocus.io>
2023-01-23 13:44:47 +0000

    Add import ordering to eslint

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,31 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
-};
+    extends: ['next', 'turbo', 'prettier'],
+    plugins: ['import'],
+    rules: {
+        '@next/next/no-html-link-for-pages': 'off',
+        'react/jsx-key': 'off',
+        'import/no-anonymous-default-export': 'off',
+        'import/order': [
+            'warn',
+            {
+                groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                pathGroups: [
+                    {
+                        pattern: '@empire/**',
+                        group: 'internal',
+                    },
+                    {
+                        pattern: '@orchid/**',
+                        group: 'external',
+                    },
+                    {
+                        pattern: 'orchid',
+                        group: 'external',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['builtin'],
+                'newlines-between': 'always',
+            },
+        ],
+    },
+}
