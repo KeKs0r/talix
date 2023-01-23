c014e9a4a500790ee32f558810cc38fb6dd0f193
KeKs0r <marc.hoeffl@laserfocus.io>
2023-01-23 12:21:13 +0000

    Add syncpack

---
+++ syncpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    indent: '    ',
+    // Seems to fix workspace:* imports
+    // https://github.com/JamieMason/syncpack/issues/95#issuecomment-1252353349
+    workspace: false,
+}
