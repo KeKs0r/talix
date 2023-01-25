5c2e9d813ab5423f286fa92d8674fce6b1203684
KeKs0r <marc.hoeffl@laserfocus.io>
2023-01-25 21:04:24 +0000

    Expose upload document url action from document service

--- packages/domain-core/src/document/index.ts
+++ packages/domain-core/src/document/index.ts
@@ -1,26 +1,38 @@
 import type { StorageAdapter } from '@castore/core'
 import { InMemoryStorageAdapter } from '@castore/inmemory-event-storage-adapter'
+import { FileStorage } from 'cf-r2-file-storage'
 import { ulid } from 'ulid'
 
 import { createDocumentCommand, CreateDocumentInput } from './document-create-command'
 import { documentEventStore } from './document-eventstore'
+import { createUploadDocumentFromUrlAction } from './upload-document-url-action'
 
 type ServiceOptions = {
     storageAdapter?: StorageAdapter
     generateId: () => string
+    fileStorage: FileStorage
 }
 
-export function createDocumentService(opts: ServiceOptions = { generateId: ulid }) {
-    const { storageAdapter, generateId } = opts || {}
-    if (storageAdapter) {
-        documentEventStore.storageAdapter = storageAdapter
-    } else {
-        documentEventStore.storageAdapter = new InMemoryStorageAdapter()
-    }
+export function createDocumentService(opts: ServiceOptions) {
+    const {
+        storageAdapter = new InMemoryStorageAdapter(),
+        generateId = ulid,
+        fileStorage,
+    } = opts || {}
+    documentEventStore.storageAdapter = storageAdapter
+
+    const createDocument = (cmd: CreateDocumentInput) =>
+        createDocumentCommand.handler(cmd, [documentEventStore], { generateId })
+
+    const uploadDocumentUrlAction = createUploadDocumentFromUrlAction({
+        createDocument,
+        fileStorage,
+        generateId,
+    })
 
     return {
-        createDocument: (cmd: CreateDocumentInput) =>
-            createDocumentCommand.handler(cmd, [documentEventStore], { generateId }),
+        createDocument,
+        uploadDocumentUrlAction,
     }
 }
 
