205b90daaf3e17d8173e776c807b9b897e4eb799
KeKs0r <marc.hoeffl@laserfocus.io>
2023-01-26 10:46:12 +0000

    stash

--- packages/domain-core/src/voucher/voucher-aggregate.ts
+++ packages/domain-core/src/voucher/voucher-aggregate.ts
@@ -3,11 +3,21 @@ import type { Aggregate } from '@castore/core'
 type DateTimeString = string
 
 export type VoucherStatus = 'DRAFT' | 'UNPAID' | 'PAID'
+export type VatTaxType = 'EU' | 'NON_EU'
 
 export interface VoucherAggregate extends Aggregate {
     status: VoucherStatus
     documentId: string
     createdAt: DateTimeString
+    updatedAt: DateTimeString
+
+    vatTaxType: VatTaxType
+    voucherDate: DateTimeString
+    deliveryDate: DateTimeString
+    creditDebit: 'CREDIT' | 'DEBIT'
+    supplierName: string
+    supplierAddress: string
+
     items: VoucherLineItem[]
 }
 
