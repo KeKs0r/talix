eed34b4315c2b2febc7246d66af6f0534f40bee1
KeKs0r <marc.hoeffl@laserfocus.io>
2023-02-25 18:55:59 +0000

    matchEvent util

---
+++ packages/chute-core/src/util/match-event.ts
@@ -0,0 +1,23 @@
+import type { Chute } from '../chute-app'
+import { EventAction, isEventAction } from '../event-action'
+
+export function matchEventAction(app: Chute, eventName: string): EventAction[] {
+    const eventActions = Object.values(app.actions).filter(isEventAction)
+    const matched = eventActions.filter((action) => {
+        return matchName(eventName, action.eventTrigger)
+    })
+    return matched
+}
+
+/**
+ * Mainly taken from
+ * https://github.com/maraisr/diary/blob/main/src/index.ts
+ */
+const to_reg_exp = (x: string) => new RegExp(x.replace(/\*/g, '.*') + '$')
+export function matchName(event: string, listener: string) {
+    const m = listener.split(/[\s,]+/).map(to_reg_exp)
+    for (let len = m.length; len--; ) {
+        if (m[len].test(event)) return true
+    }
+    return false
+}
