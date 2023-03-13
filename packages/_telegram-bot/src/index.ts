095fa0844d3d1089f41e2410ca42603ed3a5d36b
KeKs0r <marc.hoeffl@laserfocus.io>
2023-03-13 22:17:59 +0800

    Cleanup telegraf

--- packages/_telegram-bot/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { telegramPlugin as createTelegramPlugin } from './chute-telegram/telegram-plugin'
-import { DocumentUploadAction } from './handlers/tg-document'
-export type { TelegramPluginContext } from './chute-telegram/telegram-plugin'
-
-export const telegramPlugin = createTelegramPlugin([DocumentUploadAction])
--- packages/_telegram-bot/src/undici.d.ts
+++
@@ -1,6 +0,0 @@
-declare global {
-    export const { fetch, FormData, Headers, Request, Response }: typeof import('undici')
-    export type { FormData, Headers, Request, RequestInit, Response } from 'undici'
-}
-
-export {}
